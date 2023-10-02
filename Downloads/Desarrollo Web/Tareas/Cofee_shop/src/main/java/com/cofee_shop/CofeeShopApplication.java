462d8bbec8607850848685362fed55407d24fc8b
Nicole Hidalgo <nicolehidalgo437@gmail.com>
2023-10-02 16:31:10 -0600

    Semana 3 Practica 2

---
+++ Downloads/Desarrollo Web/Tareas/Cofee_shop/src/main/java/com/cofee_shop/CofeeShopApplication.java
@@ -0,0 +1,13 @@
+package com.cofee_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CofeeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CofeeShopApplication.class, args);
+	}
+
+}
---
+++ Downloads/Desarrollo Web/Tareas/Cofee_shop/src/test/java/com/cofee_shop/CofeeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cofee_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofeeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
